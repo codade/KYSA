7e46db21b20b1c45fdd36a7aa85c6a9d8494d69f
codade <58659232+codade@users.noreply.github.com>
2021-03-30 16:27:55 +0200

    Add files via upload

--- KYSA.py
+++ KYSA.py
@@ -1,141 +1,125 @@
+'''Main script coordinating the opening and closing of the GUI main window and the GUI progressbar. Furthermore the functions for data export
+plotting will be started'''
+
 import time
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GLib
 
-#multiprocessing
+#multiprocessing different Packages on Windows and Darwin
 import platform
-if platform.system()=='Windows':
-    import multiprocess as mtp
-else:
-    import multiprocessing as mtp
-
-from Base_Functions import GUI_GTK_final as guib
-from Base_Functions import plot_preprocessor as pltp
+import multiprocessing as mtp
 
 
-langdict_main=guib.langdict_all['Main_File_vars']
+    
+#-------------------------------------- Calculation and Plotting subprocess Class -----------------------------------
 
+class DataProcess(mtp.Process):
+    '''Plotting and Excel export process part'''
+    def __init__(self, queue, shutdown_event):
 
-
-#-------------------------------------- Progressbar Subprocess Class -----------------------------------
-
-class ProgressProcess(mtp.Process):
-
-    def __init__(self,queue):
-        #initalize class as process
         mtp.Process.__init__(self)
-        self.queue=queue
 
-    def queue_check(self):
-        #continously check queue pipeline if the calculation process sent information relevant for progressbar behavior
-        try:
-            msg,data=self.queue.get(0) #get tuple of latest queue entry. msg states type of action (process start/error/end or show progressbar)
+        self.queue = queue
+        self.accounts_data = None
+        self.plot_data = None
+        self.continue_plot = False
+        self.shutdown_event = shutdown_event
 
-            if 'process' in msg:
-                self.progress.userinformation(msg,data)
-                self.queue.put(('success',""))
-            elif 'show' in msg:
-                self.progress.show_all()
-        except:
-            pass
-
-        return True
-
-    def run(self): #start showing progress bar
-        import gi
-        from gi.repository import Gtk, GLib
-
-        self.progress = guib.ProgressBarWindow()
-
-        #set timeout for queue checking (timeout means every 100ms the function will be called)
-        GLib.timeout_add(100, self.queue_check)
-        self.progress.hide()#hide progressbar until plotting Process is started (necessary for different multiprocess handling in Windows and Linux)
-        Gtk.main()
-
-
-#-------------------------------------- Calculation and Plotting mainprocess Class -----------------------------------
-
-class PlottingProcess():
-
-    def __init__(self,queue,accounts_data):
-
+    def run(self):    
         
-        self.queue=queue
-        self.accounts_data=accounts_data
-        self.plot_data=pltp.Plotters(self.accounts_data)
-        self.queue.put(('show','')) #show progressbar
+        self.queue_waiting()
 
-        
+    def queue_waiting(self):
+        '''function to constantly check for signal words (msg) in queue'''
+       
+        while not self.shutdown_event.is_set():
+            try:
+                msg, data = self.queue.get(block=True, timeout=0.15)
 
-    def start_process(self):
-        #process script executed as soon as class gets started
+            except Exception:
+                continue
+
+            if msg == 'rawimport_start':
+
+                self.import_rawdata(data)
+                break
+
+            elif msg == 'start_export':
+                
+                self.start_exportprocess(data)
+                break
+                
+            elif msg == 'success':
+                break
+
+    def import_rawdata(self, data): 
+
+
+        accounts_data, filenames, importtype = data
+        self.accounts_data = accounts_data
+
+        for item in filenames:
+            self.accounts_data.process_data(item, importtype)
+
+        self.queue.put(('rawimport_end', self.accounts_data))
+        time.sleep(0.3)
+        self.queue_waiting()
+
+    def start_exportprocess(self, data):
+        '''process script executed as soon as class gets started'''
+        from basefunctions import plot_preprocessor as pltp
         
+        #hide icon in Mac which is generated after import
+        if platform.system() == 'Darwin':
+            import AppKit 
+            AppKit.NSApp.setActivationPolicy_(2)
+
+        self.accounts_data = data
+        self.plot_data =pltp.Plotters(self.accounts_data)
+
         self.data_pre_visualisation()
         if self.continue_plot:
             self.data_visualisation()
+        self.shutdown_event.set()
 
 
-    def queue_waiting(self):
-        # as this process isn't embedded within a GUI a while-loop has to be used to check for queue entries sent from progressbar process 
-        # This construction is used provisionally until a better and stable solution is found. Progressbar returns msg=success after process action(start/error/end) has been executed
-
-        while True:
-            
-            try:
-                msg,data=self.queue.get(0)
-                if msg=='success':
-                    break
-               
-            except:
-                continue
-
-
-#################################### Data Excel Export and Plotting Halt/Start #################################  
+#################################### Data Excel Export and Plotting Halt/Start #################################
     def data_pre_visualisation(self):
-        #export datasets to excel. if successful continue with plotting; if not stop processes
-        
-        try:
-            self.accounts_data.makefolder_excel() 
-            self.continue_plot=True #process to next section
+        '''export datasets to excel. if successful continue with plotting; if not stop processes'''
 
-            info_text=(langdict_main['data_prep_succ'][0],langdict_main['data_prep_succ'][1])
-            self.queue.put(('process_start',info_text)) #send process continuation message to progressbar process and wait until "success" is reported back
-            time.sleep(0.5)
+        try:
+            self.accounts_data.makefolder_excel()
+            self.continue_plot = True #process to next section
+
+            self.queue.put(('process_start', 'data_prep_succ')) #send process continuation message to progressbar process and wait until "success" is reported back
+            time.sleep(0.3)
             self.queue_waiting()
 
         except:
-            info_text=(langdict_main['data_prep_err'][0],langdict_main['data_prep_err'][1])
-            self.continue_plot=False #stop programm
-            self.queue.put(('process_error',info_text)) #send error message to progressbar and stop program (in porgressbar process)
- 
-     
+            self.continue_plot = False #stop programm
+            self.queue.put(('process_error', 'data_prep_err7')) #send error message to progressbar and stop program (in porgressbar process)
 
-#################################### Data Plotting Part #################################     
+
+
+
+#################################### Data Plotting Part #################################
     def data_visualisation(self):
+        '''prepare data to be plotted and execute plotting'''
 
-    #prepare data to be plotted and execute plotting
         while True:
             #prepare plot data. If successful start plotting. Else stop program
 
             try:
-                self.plot_data.makeplotdata() 
-            except:
-                info_text=(langdict_main['data_vis_err5'][0],langdict_main['data_vis_err5'][1])
-                self.queue.put(('process_error',info_text))  #send error message to progressbar and stop program (in porgressbar process)
-
+                self.plot_data.makeplotdata()
+            except:                
+                self.queue.put(('process_error', 'data_vis_err8'))  #send error message to progressbar and stop program (in porgressbar process)
                 break
-
             #Create Plots and save 'pngs'
 
             try:
                 self.plot_data.plotdata()
-                info_text=(langdict_main['data_vis_succ'][0],eval(f"f'''{langdict_main['data_vis_succ'][1]}'''")) #eval function transforms f-string statements to be interpreted as f-strings
-                self.queue.put(('process_end',info_text))  #send successful data analysis message to progressbar and end program (in porgressbar process)
+                self.queue.put(('process_end', 'data_vis_succ'))  #send successful data analysis message to progressbar and end program (in porgressbar process)
 
             except:
-                info_text=(langdict_main['data_vis_err6'][0],langdict_main['data_vis_err6'][1])
-                self.queue.put(('process_error',info_text)) #send error message to progressbar and stop program (in porgressbar process)
+                self.queue.put(('process_error', 'data_vis_err9')) #send error message to progressbar and stop program (in porgressbar process)
             break
 
 
@@ -144,36 +128,26 @@ class PlottingProcess():
 
 if __name__ == '__main__':
 
-    mtp.freeze_support() #necessary for windows
+    mtp.freeze_support() #necessary for Windows and Darwin to stop building Gui Windows
 
     #set variables
-    progressqueue=mtp.Queue()
-    progressbar = ProgressProcess(progressqueue)
+    progressqueue = mtp.Queue() #queue needed for the communication between the processes  
+    shutdown_event = mtp.Event() #create shutdown event to be used as information about process state in both processes
+
+    data_proc = DataProcess(progressqueue, shutdown_event) #initalize data process class
 
 
-    # necessary differentiation between windows and Linux, as Widnows handles multiprocessing differently (processes are spawned, that's why subprocess has to be started at beginning)
-    if platform.system()=='Windows':
-        #start hidden progressbar befor Main GUI
-        progressbar.start()        
-        win = guib.Main_Window()
-        Gtk.main()
-        
-        # Proceed with calculation after Main GUi was closed through script for data analysis 
-        if win.proceed:
-            plot_process=PlottingProcess(progressqueue, win.mfunc.accounts_data)
-            plot_process.start_process()
+    data_proc.start()
 
-        #if Main GUI was closed through user via buttons "quit" or "cancel"
-        else:
-            progressbar.terminate()
+    #dictionary for user messages posted during plot processing 
+    from basefunctions import gui_classes as guicl
+    import gi
+    gi.require_version('Gtk', '3.0')
+    from gi.repository import Gtk, GLib
 
-    #Linux forks multiple processed. That's why the progressbar process can be started after the Main Window was closed
-    elif platform.system()=='Linux':
-        win = guib.Main_Window()
-        Gtk.main()
-        if win.proceed:
-            progressbar.start()
-            plot_process=PlottingProcess(progressqueue, win.mfunc.accounts_data)
-            plot_process.start_process()
-        else:
-        	pass
+    #start hidden progressbar process before starting Main GUI
+    
+    win = guicl.MainWindow(progressqueue)
+    Gtk.main()
+    data_proc.terminate()
+    data_proc.join()
